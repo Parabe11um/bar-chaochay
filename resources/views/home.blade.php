e744367bd21e2069fb71b853ea1c1d154c266c0b
Parabe11um <r.a.bazhenoff@gmail.com>
2025-06-17 01:17:06 +0300

    slider images popups added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -129,7 +129,7 @@
                     <div class="flex flex-wrap mx-[-15px]">
                         <div class="lg:w-10/12 xl:w-8/12 xxl:w-7/12 w-full flex-[0_0_auto] !px-[15px] max-w-full !mx-auto !mb-8">
                             <h2 class="xl:!text-[1.7rem] !text-[calc(1.295rem_+_0.54vw)] !leading-[1.25] !font-semibold !mb-3">Наш чай</h2>
-                            <p class="lead !text-[1.1rem] !leading-[1.55] font-medium">Photography is my passion and I love to turn ideas into beautiful things.</p>
+                            <p class="lead !text-[1.1rem] !leading-[1.55] font-medium">Мы предлагаем весь спектр китайского чая.</p>
                         </div>
                         <!-- /column -->
                     </div>
@@ -138,7 +138,7 @@
                         <div class="isotope-filter !relative z-[5] filter !mb-10">
                             <ul class=" inline m-0 p-0 list-none">
                                 <li class="inline"><a class="filter-item uppercase !tracking-[0.02rem] text-[0.7rem] font-bold !text-[#aab0bc] cursor-pointer hover:!text-[#28a745] active" data-filter="*">Все</a></li>
-                                <li class="inline before:content-[''] before:inline-block before:w-[0.2rem] before:h-[0.2rem] before:ml-2 before:mr-[0.8rem] before:my-0 before:rounded-[100%] before:bg-[rgba(30,34,40,.2)] before:align-[.15rem]"><a class="filter-item uppercase !tracking-[0.02rem] text-[0.7rem] font-bold !text-[#aab0bc] cursor-pointer hover:!text-[#28a745]" data-filter=".foods">Красный чай</a></li>
+                                <li class="inline before:content-[''] before:inline-block before:w-[0.2rem] before:h-[0.2rem] before:ml-2 before:mr-[0.8rem] before:my-0 before:rounded-[100%] before:bg-[rgba(30,34,40,.2)] before:align-[.15rem]"><a class="filter-item uppercase !tracking-[0.02rem] text-[0.7rem] font-bold !text-[#aab0bc] cursor-pointer hover:!text-[#28a745]" data-filter=".red">Красный чай</a></li>
                                 <li class="inline before:content-[''] before:inline-block before:w-[0.2rem] before:h-[0.2rem] before:ml-2 before:mr-[0.8rem] before:my-0 before:rounded-[100%] before:bg-[rgba(30,34,40,.2)] before:align-[.15rem]"><a class="filter-item uppercase !tracking-[0.02rem] text-[0.7rem] font-bold !text-[#aab0bc] cursor-pointer hover:!text-[#28a745]" data-filter=".drinks">Белый чай</a></li>
                                 <li class="inline before:content-[''] before:inline-block before:w-[0.2rem] before:h-[0.2rem] before:ml-2 before:mr-[0.8rem] before:my-0 before:rounded-[100%] before:bg-[rgba(30,34,40,.2)] before:align-[.15rem]"><a class="filter-item uppercase !tracking-[0.02rem] text-[0.7rem] font-bold !text-[#aab0bc] cursor-pointer hover:!text-[#28a745]" data-filter=".events">Пуэр</a></li>
                                 <li class="inline before:content-[''] before:inline-block before:w-[0.2rem] before:h-[0.2rem] before:ml-2 before:mr-[0.8rem] before:my-0 before:rounded-[100%] before:bg-[rgba(30,34,40,.2)] before:align-[.15rem]"><a class="filter-item uppercase !tracking-[0.02rem] text-[0.7rem] font-bold !text-[#aab0bc] cursor-pointer hover:!text-[#28a745]" data-filter=".pastries">Зеленый чай</a></li>
@@ -146,7 +146,7 @@
                         </div>
                         <div class="flex flex-wrap mx-[-15px] md:mx-[-15px] !mt-[-30px] isotope">
                             <div class="project item xl:w-4/12 lg:w-6/12 md:w-6/12 w-full flex-[0_0_auto] !px-[15px] !mt-[30px] max-w-full drinks events">
-                                <figure class="overlay overlay-1 rounded group relative"><a class="relative block z-[3] cursor-pointer inset-0" href="./assets/img/photos/pf1-full.jpg" data-glightbox="" data-gallery="shots-group"> <img src="{{ asset('images/pf1.jpg') }}" alt="image"></a>
+                                <figure class="overlay overlay-1 rounded group relative"><a class="relative block z-[3] cursor-pointer inset-0" href="{{ asset('images/img.png') }}" data-glightbox="" data-gallery="shots-group"> <img src="{{ asset('images/img.png') }}" alt="image"></a>
                                     <figcaption class="group-hover:opacity-100 absolute w-full h-full opacity-0 text-center px-4 py-3 inset-0 z-[5] pointer-events-none p-2">
                                         <h5 class="from-top !mb-0 absolute w-full translate-y-[-80%] px-4 py-3 left-0 top-2/4 group-hover:-translate-y-2/4">Fringilla Nullam</h5>
                                     </figcaption>
@@ -161,7 +161,7 @@
                                 </figure>
                             </div>
                             <!-- /.project -->
-                            <div class="project item xl:w-4/12 lg:w-6/12 md:w-6/12 w-full flex-[0_0_auto] !px-[15px] !mt-[30px] max-w-full drinks foods">
+                            <div class="project item xl:w-4/12 lg:w-6/12 md:w-6/12 w-full flex-[0_0_auto] !px-[15px] !mt-[30px] max-w-full drinks red">
                                 <figure class="overlay overlay-1 rounded group relative"><a class="relative block z-[3] cursor-pointer inset-0" href="./assets/img/photos/pf3-full.jpg" data-glightbox="" data-gallery="shots-group"> <img src="{{ asset('images/pf3.jpg') }}" alt="image"></a>
                                     <figcaption class="group-hover:opacity-100 absolute w-full h-full opacity-0 text-center px-4 py-3 inset-0 z-[5] pointer-events-none p-2">
                                         <h5 class="from-top !mb-0 absolute w-full translate-y-[-80%] px-4 py-3 left-0 top-2/4 group-hover:-translate-y-2/4">Ornare Ipsum</h5>
@@ -185,7 +185,7 @@
                                 </figure>
                             </div>
                             <!-- /.project -->
-                            <div class="project item xl:w-4/12 lg:w-6/12 md:w-6/12 w-full flex-[0_0_auto] !px-[15px] !mt-[30px] max-w-full foods">
+                            <div class="project item xl:w-4/12 lg:w-6/12 md:w-6/12 w-full flex-[0_0_auto] !px-[15px] !mt-[30px] max-w-full red">
                                 <figure class="overlay overlay-1 rounded group relative"><a class="relative block z-[3] cursor-pointer inset-0" href="./assets/img/photos/pf6-full.jpg" data-glightbox="" data-gallery="shots-group"> <img src="{{ asset('images/pf6.jpg') }}" alt="image"></a>
                                     <figcaption class="group-hover:opacity-100 absolute w-full h-full opacity-0 text-center px-4 py-3 inset-0 z-[5] pointer-events-none p-2">
                                         <h5 class="from-top !mb-0 absolute w-full translate-y-[-80%] px-4 py-3 left-0 top-2/4 group-hover:-translate-y-2/4">Ridiculus Tristique</h5>
@@ -193,7 +193,7 @@
                                 </figure>
                             </div>
                             <!-- /.project -->
-                            <div class="project item xl:w-4/12 lg:w-6/12 md:w-6/12 w-full flex-[0_0_auto] !px-[15px] !mt-[30px] max-w-full foods drinks">
+                            <div class="project item xl:w-4/12 lg:w-6/12 md:w-6/12 w-full flex-[0_0_auto] !px-[15px] !mt-[30px] max-w-full red drinks">
                                 <figure class="overlay overlay-1 rounded group relative"><a class="relative block z-[3] cursor-pointer inset-0" href="./assets/img/photos/pf7-full.jpg" data-glightbox="" data-gallery="shots-group"> <img src="{{ asset('images/pf7.jpg') }}" alt="image"></a>
                                     <figcaption class="group-hover:opacity-100 absolute w-full h-full opacity-0 text-center px-4 py-3 inset-0 z-[5] pointer-events-none p-2">
                                         <h5 class="from-top !mb-0 absolute w-full translate-y-[-80%] px-4 py-3 left-0 top-2/4 group-hover:-translate-y-2/4">Sollicitudin Pharetra</h5>
@@ -217,7 +217,7 @@
                                 </figure>
                             </div>
                             <!-- /.project -->
-                            <div class="project item xl:w-4/12 lg:w-6/12 md:w-6/12 w-full flex-[0_0_auto] !px-[15px] !mt-[30px] max-w-full foods">
+                            <div class="project item xl:w-4/12 lg:w-6/12 md:w-6/12 w-full flex-[0_0_auto] !px-[15px] !mt-[30px] max-w-full red">
                                 <figure class="overlay overlay-1 rounded group relative"><a class="relative block z-[3] cursor-pointer inset-0" href="./assets/img/photos/pf10-full.jpg" data-glightbox="" data-gallery="shots-group"> <img src="{{ asset('images/pf10.jpg') }}" alt="image"></a>
                                     <figcaption class="group-hover:opacity-100 absolute w-full h-full opacity-0 text-center px-4 py-3 inset-0 z-[5] pointer-events-none p-2">
                                         <h5 class="from-top !mb-0 absolute w-full translate-y-[-80%] px-4 py-3 left-0 top-2/4 group-hover:-translate-y-2/4">Consectetur Malesuada</h5>
@@ -233,7 +233,7 @@
                                 </figure>
                             </div>
                             <!-- /.project -->
-                            <div class="project item xl:w-4/12 lg:w-6/12 md:w-6/12 w-full flex-[0_0_auto] !px-[15px] !mt-[30px] max-w-full foods">
+                            <div class="project item xl:w-4/12 lg:w-6/12 md:w-6/12 w-full flex-[0_0_auto] !px-[15px] !mt-[30px] max-w-full red">
                                 <figure class="overlay overlay-1 rounded group relative"><a class="relative block z-[3] cursor-pointer inset-0" href="./assets/img/photos/pf12-full.jpg" data-glightbox="" data-gallery="shots-group"> <img src="{{ asset('images/pf12.jpg') }}" alt="image"></a>
                                     <figcaption class="group-hover:opacity-100 absolute w-full h-full opacity-0 text-center px-4 py-3 inset-0 z-[5] pointer-events-none p-2">
                                         <h5 class="from-top !mb-0 absolute w-full translate-y-[-80%] px-4 py-3 left-0 top-2/4 group-hover:-translate-y-2/4">Pellentesque Commodo</h5>

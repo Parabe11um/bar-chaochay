ae8b345dceef4318a5cec09205f9a3c54cbed185
Parabe11um <r.a.bazhenoff@gmail.com>
2025-07-02 17:49:25 +0300

    Photo added to the slider

--- resources/views/layouts/partials/slider.blade.php
+++ resources/views/layouts/partials/slider.blade.php
@@ -1,7 +1,7 @@
 <section id="home">
     <div class="wrapper  bg-[rgba(246,247,249,1)]  overflow-hidden">
         <div class="container-fluid xl:!px-0 pt-6 pb-10">
-            <div class="swiper-container swiper-auto relative z-10" data-margin="30" data-dots="true" data-nav="true" data-centered="false" data-loop="true" data-items-auto="true">
+            <div class="swiper-container swiper-auto relative z-10" data-margin="30" data-dots="false" data-nav="true" data-centered="false" data-loop="true" data-items-auto="true">
                 <div class="swiper overflow-visible">
                     <div class="swiper-wrapper">
                         <div class="swiper-slide group">
@@ -26,6 +26,70 @@
                         <!--/.swiper-slide -->
                         <div class="swiper-slide group">
                             <figure class="rounded-[0.4rem]"><img class="rounded-[0.4rem]" src="{{ asset('images/6.jpg') }}" alt="image"><a class="item-link absolute w-[2.2rem] h-[2.2rem] !leading-[2.2rem] z-[1] transition-all duration-[0.3s] ease-in-out opacity-0 !text-[#343f52] shadow-[0_0.25rem_0.75rem_rgba(30,34,40,0.02)] text-[1rem] flex items-center justify-center rounded-[100%] right-0 bottom-4 bg-[rgba(255,255,255,.7)] hover:bg-[rgba(255,255,255,.9)] hover:!text-[#343f52] group-hover:opacity-100 group-hover:right-[1rem]" href="{{ asset('images/6.jpg') }}" data-glightbox="" data-gallery="gallery-group"><i class="uil uil-focus-add before:content-['\eb22']"></i></a></figure>
+                        </div>
+                        <!--/.swiper-slide -->
+                        <div class="swiper-slide group">
+                            <figure class="rounded-[0.4rem]"><img class="rounded-[0.4rem]" src="{{ asset('images/7.jpg') }}" alt="image"><a class="item-link absolute w-[2.2rem] h-[2.2rem] !leading-[2.2rem] z-[1] transition-all duration-[0.3s] ease-in-out opacity-0 !text-[#343f52] shadow-[0_0.25rem_0.75rem_rgba(30,34,40,0.02)] text-[1rem] flex items-center justify-center rounded-[100%] right-0 bottom-4 bg-[rgba(255,255,255,.7)] hover:bg-[rgba(255,255,255,.9)] hover:!text-[#343f52] group-hover:opacity-100 group-hover:right-[1rem]" href="{{ asset('images/7.jpg') }}" data-glightbox="" data-gallery="gallery-group"><i class="uil uil-focus-add before:content-['\eb22']"></i></a></figure>
+                        </div>
+                        <!--/.swiper-slide -->
+                        <div class="swiper-slide group">
+                            <figure class="rounded-[0.4rem]"><img class="rounded-[0.4rem]" src="{{ asset('images/8.jpg') }}" alt="image"><a class="item-link absolute w-[2.2rem] h-[2.2rem] !leading-[2.2rem] z-[1] transition-all duration-[0.3s] ease-in-out opacity-0 !text-[#343f52] shadow-[0_0.25rem_0.75rem_rgba(30,34,40,0.02)] text-[1rem] flex items-center justify-center rounded-[100%] right-0 bottom-4 bg-[rgba(255,255,255,.7)] hover:bg-[rgba(255,255,255,.9)] hover:!text-[#343f52] group-hover:opacity-100 group-hover:right-[1rem]" href="{{ asset('images/8.jpg') }}" data-glightbox="" data-gallery="gallery-group"><i class="uil uil-focus-add before:content-['\eb22']"></i></a></figure>
+                        </div>
+                        <!--/.swiper-slide -->
+                        <div class="swiper-slide group">
+                            <figure class="rounded-[0.4rem]"><img class="rounded-[0.4rem]" src="{{ asset('images/9.jpg') }}" alt="image"><a class="item-link absolute w-[2.2rem] h-[2.2rem] !leading-[2.2rem] z-[1] transition-all duration-[0.3s] ease-in-out opacity-0 !text-[#343f52] shadow-[0_0.25rem_0.75rem_rgba(30,34,40,0.02)] text-[1rem] flex items-center justify-center rounded-[100%] right-0 bottom-4 bg-[rgba(255,255,255,.7)] hover:bg-[rgba(255,255,255,.9)] hover:!text-[#343f52] group-hover:opacity-100 group-hover:right-[1rem]" href="{{ asset('images/9.jpg') }}" data-glightbox="" data-gallery="gallery-group"><i class="uil uil-focus-add before:content-['\eb22']"></i></a></figure>
+                        </div>
+                        <!--/.swiper-slide -->
+                        <div class="swiper-slide group">
+                            <figure class="rounded-[0.4rem]"><img class="rounded-[0.4rem]" src="{{ asset('images/10.jpg') }}" alt="image"><a class="item-link absolute w-[2.2rem] h-[2.2rem] !leading-[2.2rem] z-[1] transition-all duration-[0.3s] ease-in-out opacity-0 !text-[#343f52] shadow-[0_0.25rem_0.75rem_rgba(30,34,40,0.02)] text-[1rem] flex items-center justify-center rounded-[100%] right-0 bottom-4 bg-[rgba(255,255,255,.7)] hover:bg-[rgba(255,255,255,.9)] hover:!text-[#343f52] group-hover:opacity-100 group-hover:right-[1rem]" href="{{ asset('images/10.jpg') }}" data-glightbox="" data-gallery="gallery-group"><i class="uil uil-focus-add before:content-['\eb22']"></i></a></figure>
+                        </div>
+                        <!--/.swiper-slide -->
+                        <div class="swiper-slide group">
+                            <figure class="rounded-[0.4rem]"><img class="rounded-[0.4rem]" src="{{ asset('images/11.jpg') }}" alt="image"><a class="item-link absolute w-[2.2rem] h-[2.2rem] !leading-[2.2rem] z-[1] transition-all duration-[0.3s] ease-in-out opacity-0 !text-[#343f52] shadow-[0_0.25rem_0.75rem_rgba(30,34,40,0.02)] text-[1rem] flex items-center justify-center rounded-[100%] right-0 bottom-4 bg-[rgba(255,255,255,.7)] hover:bg-[rgba(255,255,255,.9)] hover:!text-[#343f52] group-hover:opacity-100 group-hover:right-[1rem]" href="{{ asset('images/11.jpg') }}" data-glightbox="" data-gallery="gallery-group"><i class="uil uil-focus-add before:content-['\eb22']"></i></a></figure>
+                        </div>
+                        <!--/.swiper-slide -->
+                        <div class="swiper-slide group">
+                            <figure class="rounded-[0.4rem]"><img class="rounded-[0.4rem]" src="{{ asset('images/12.jpg') }}" alt="image"><a class="item-link absolute w-[2.2rem] h-[2.2rem] !leading-[2.2rem] z-[1] transition-all duration-[0.3s] ease-in-out opacity-0 !text-[#343f52] shadow-[0_0.25rem_0.75rem_rgba(30,34,40,0.02)] text-[1rem] flex items-center justify-center rounded-[100%] right-0 bottom-4 bg-[rgba(255,255,255,.7)] hover:bg-[rgba(255,255,255,.9)] hover:!text-[#343f52] group-hover:opacity-100 group-hover:right-[1rem]" href="{{ asset('images/12.jpg') }}" data-glightbox="" data-gallery="gallery-group"><i class="uil uil-focus-add before:content-['\eb22']"></i></a></figure>
+                        </div>
+                        <!--/.swiper-slide -->
+                        <div class="swiper-slide group">
+                            <figure class="rounded-[0.4rem]"><img class="rounded-[0.4rem]" src="{{ asset('images/13.jpg') }}" alt="image"><a class="item-link absolute w-[2.2rem] h-[2.2rem] !leading-[2.2rem] z-[1] transition-all duration-[0.3s] ease-in-out opacity-0 !text-[#343f52] shadow-[0_0.25rem_0.75rem_rgba(30,34,40,0.02)] text-[1rem] flex items-center justify-center rounded-[100%] right-0 bottom-4 bg-[rgba(255,255,255,.7)] hover:bg-[rgba(255,255,255,.9)] hover:!text-[#343f52] group-hover:opacity-100 group-hover:right-[1rem]" href="{{ asset('images/13.jpg') }}" data-glightbox="" data-gallery="gallery-group"><i class="uil uil-focus-add before:content-['\eb22']"></i></a></figure>
+                        </div>
+                        <!--/.swiper-slide -->
+                        <div class="swiper-slide group">
+                            <figure class="rounded-[0.4rem]"><img class="rounded-[0.4rem]" src="{{ asset('images/14.jpg') }}" alt="image"><a class="item-link absolute w-[2.2rem] h-[2.2rem] !leading-[2.2rem] z-[1] transition-all duration-[0.3s] ease-in-out opacity-0 !text-[#343f52] shadow-[0_0.25rem_0.75rem_rgba(30,34,40,0.02)] text-[1rem] flex items-center justify-center rounded-[100%] right-0 bottom-4 bg-[rgba(255,255,255,.7)] hover:bg-[rgba(255,255,255,.9)] hover:!text-[#343f52] group-hover:opacity-100 group-hover:right-[1rem]" href="{{ asset('images/14.jpg') }}" data-glightbox="" data-gallery="gallery-group"><i class="uil uil-focus-add before:content-['\eb22']"></i></a></figure>
+                        </div>
+                        <!--/.swiper-slide -->
+                        <div class="swiper-slide group">
+                            <figure class="rounded-[0.4rem]"><img class="rounded-[0.4rem]" src="{{ asset('images/15.jpg') }}" alt="image"><a class="item-link absolute w-[2.2rem] h-[2.2rem] !leading-[2.2rem] z-[1] transition-all duration-[0.3s] ease-in-out opacity-0 !text-[#343f52] shadow-[0_0.25rem_0.75rem_rgba(30,34,40,0.02)] text-[1rem] flex items-center justify-center rounded-[100%] right-0 bottom-4 bg-[rgba(255,255,255,.7)] hover:bg-[rgba(255,255,255,.9)] hover:!text-[#343f52] group-hover:opacity-100 group-hover:right-[1rem]" href="{{ asset('images/15.jpg') }}" data-glightbox="" data-gallery="gallery-group"><i class="uil uil-focus-add before:content-['\eb22']"></i></a></figure>
+                        </div>
+                        <!--/.swiper-slide -->
+                        <div class="swiper-slide group">
+                            <figure class="rounded-[0.4rem]"><img class="rounded-[0.4rem]" src="{{ asset('images/16.jpg') }}" alt="image"><a class="item-link absolute w-[2.2rem] h-[2.2rem] !leading-[2.2rem] z-[1] transition-all duration-[0.3s] ease-in-out opacity-0 !text-[#343f52] shadow-[0_0.25rem_0.75rem_rgba(30,34,40,0.02)] text-[1rem] flex items-center justify-center rounded-[100%] right-0 bottom-4 bg-[rgba(255,255,255,.7)] hover:bg-[rgba(255,255,255,.9)] hover:!text-[#343f52] group-hover:opacity-100 group-hover:right-[1rem]" href="{{ asset('images/16.jpg') }}" data-glightbox="" data-gallery="gallery-group"><i class="uil uil-focus-add before:content-['\eb22']"></i></a></figure>
+                        </div>
+                        <!--/.swiper-slide -->
+                        <div class="swiper-slide group">
+                            <figure class="rounded-[0.4rem]"><img class="rounded-[0.4rem]" src="{{ asset('images/17.jpg') }}" alt="image"><a class="item-link absolute w-[2.2rem] h-[2.2rem] !leading-[2.2rem] z-[1] transition-all duration-[0.3s] ease-in-out opacity-0 !text-[#343f52] shadow-[0_0.25rem_0.75rem_rgba(30,34,40,0.02)] text-[1rem] flex items-center justify-center rounded-[100%] right-0 bottom-4 bg-[rgba(255,255,255,.7)] hover:bg-[rgba(255,255,255,.9)] hover:!text-[#343f52] group-hover:opacity-100 group-hover:right-[1rem]" href="{{ asset('images/17.jpg') }}" data-glightbox="" data-gallery="gallery-group"><i class="uil uil-focus-add before:content-['\eb22']"></i></a></figure>
+                        </div>
+                        <!--/.swiper-slide -->
+                        <div class="swiper-slide group">
+                            <figure class="rounded-[0.4rem]"><img class="rounded-[0.4rem]" src="{{ asset('images/18.jpg') }}" alt="image"><a class="item-link absolute w-[2.2rem] h-[2.2rem] !leading-[2.2rem] z-[1] transition-all duration-[0.3s] ease-in-out opacity-0 !text-[#343f52] shadow-[0_0.25rem_0.75rem_rgba(30,34,40,0.02)] text-[1rem] flex items-center justify-center rounded-[100%] right-0 bottom-4 bg-[rgba(255,255,255,.7)] hover:bg-[rgba(255,255,255,.9)] hover:!text-[#343f52] group-hover:opacity-100 group-hover:right-[1rem]" href="{{ asset('images/18.jpg') }}" data-glightbox="" data-gallery="gallery-group"><i class="uil uil-focus-add before:content-['\eb22']"></i></a></figure>
+                        </div>
+                        <!--/.swiper-slide -->
+                        <div class="swiper-slide group">
+                            <figure class="rounded-[0.4rem]"><img class="rounded-[0.4rem]" src="{{ asset('images/19.jpg') }}" alt="image"><a class="item-link absolute w-[2.2rem] h-[2.2rem] !leading-[2.2rem] z-[1] transition-all duration-[0.3s] ease-in-out opacity-0 !text-[#343f52] shadow-[0_0.25rem_0.75rem_rgba(30,34,40,0.02)] text-[1rem] flex items-center justify-center rounded-[100%] right-0 bottom-4 bg-[rgba(255,255,255,.7)] hover:bg-[rgba(255,255,255,.9)] hover:!text-[#343f52] group-hover:opacity-100 group-hover:right-[1rem]" href="{{ asset('images/19.jpg') }}" data-glightbox="" data-gallery="gallery-group"><i class="uil uil-focus-add before:content-['\eb22']"></i></a></figure>
+                        </div>
+                        <!--/.swiper-slide -->
+                        <div class="swiper-slide group">
+                            <figure class="rounded-[0.4rem]"><img class="rounded-[0.4rem]" src="{{ asset('images/20.jpg') }}" alt="image"><a class="item-link absolute w-[2.2rem] h-[2.2rem] !leading-[2.2rem] z-[1] transition-all duration-[0.3s] ease-in-out opacity-0 !text-[#343f52] shadow-[0_0.25rem_0.75rem_rgba(30,34,40,0.02)] text-[1rem] flex items-center justify-center rounded-[100%] right-0 bottom-4 bg-[rgba(255,255,255,.7)] hover:bg-[rgba(255,255,255,.9)] hover:!text-[#343f52] group-hover:opacity-100 group-hover:right-[1rem]" href="{{ asset('images/20.jpg') }}" data-glightbox="" data-gallery="gallery-group"><i class="uil uil-focus-add before:content-['\eb22']"></i></a></figure>
+                        </div>
+                        <!--/.swiper-slide -->
+                        <div class="swiper-slide group">
+                            <figure class="rounded-[0.4rem]"><img class="rounded-[0.4rem]" src="{{ asset('images/21.jpg') }}" alt="image"><a class="item-link absolute w-[2.2rem] h-[2.2rem] !leading-[2.2rem] z-[1] transition-all duration-[0.3s] ease-in-out opacity-0 !text-[#343f52] shadow-[0_0.25rem_0.75rem_rgba(30,34,40,0.02)] text-[1rem] flex items-center justify-center rounded-[100%] right-0 bottom-4 bg-[rgba(255,255,255,.7)] hover:bg-[rgba(255,255,255,.9)] hover:!text-[#343f52] group-hover:opacity-100 group-hover:right-[1rem]" href="{{ asset('images/21.jpg') }}" data-glightbox="" data-gallery="gallery-group"><i class="uil uil-focus-add before:content-['\eb22']"></i></a></figure>
+                        </div>
+                        <!--/.swiper-slide -->
+                        <div class="swiper-slide group">
+                            <figure class="rounded-[0.4rem]"><img class="rounded-[0.4rem]" src="{{ asset('images/22.jpg') }}" alt="image"><a class="item-link absolute w-[2.2rem] h-[2.2rem] !leading-[2.2rem] z-[1] transition-all duration-[0.3s] ease-in-out opacity-0 !text-[#343f52] shadow-[0_0.25rem_0.75rem_rgba(30,34,40,0.02)] text-[1rem] flex items-center justify-center rounded-[100%] right-0 bottom-4 bg-[rgba(255,255,255,.7)] hover:bg-[rgba(255,255,255,.9)] hover:!text-[#343f52] group-hover:opacity-100 group-hover:right-[1rem]" href="{{ asset('images/22.jpg') }}" data-glightbox="" data-gallery="gallery-group"><i class="uil uil-focus-add before:content-['\eb22']"></i></a></figure>
                         </div>
                         <!--/.swiper-slide -->
 

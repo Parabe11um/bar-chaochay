cfae956857f1a9d473c977cb6df226cd82b40915
Parabe11um <r.a.bazhenoff@gmail.com>
2025-07-10 00:55:41 +0300

    Yandex map added

--- resources/views/layouts/partials/map.blade.php
+++ resources/views/layouts/partials/map.blade.php
@@ -5,27 +5,5 @@
         frameborder="0"
         allowfullscreen="true">
     </iframe>
+
 </section>
-
-@push('scripts')
-    <script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>
-    <script>
-        ymaps.ready(function () {
-            var myMap = new ymaps.Map("yandex-map", {
-                center: [55.751244, 37.618423],
-                zoom: 16,
-                controls: ['zoomControl']
-            });
-
-            var myPlacemark = new ymaps.Placemark([55.751244, 37.618423], {
-                hintContent: 'ЧаоЧай',
-                balloonContent: 'Мы здесь!'
-            }, {
-                preset: 'islands#icon',
-                iconColor: '#ff6600'
-            });
-
-            myMap.geoObjects.add(myPlacemark);
-        });
-    </script>
-@endpush

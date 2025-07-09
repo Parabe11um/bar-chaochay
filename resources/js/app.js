6cadc5b14e752ff7eacc70a37b04973c352bbdf1
Parabe11um <r.a.bazhenoff@gmail.com>
2025-07-09 17:25:11 +0300

    First screen fixed

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,8 +4,27 @@ import 'swiper/css/bundle';
 import Alpine from 'alpinejs'
 
 window.Alpine = Alpine
+
+Alpine.data('typeEffect', (text, speed = 50) => ({
+    full: text,
+    output: '',
+    index: 0,
+    startTyping() {
+        this.output = '';
+        this.index = 0;
+        const step = () => {
+            if (this.index < this.full.length) {
+                this.output += this.full[this.index++];
+                setTimeout(step, speed);
+            }
+        };
+        step();
+    }
+}));
+
 Alpine.start()
 window.Swiper = Swiper;
 
 window.axios.defaults.headers.common['X-CSRF-TOKEN'] =
     document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+

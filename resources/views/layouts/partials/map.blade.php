959d58c411c5a851732a4c0a219a15d8cbfd6e82
Parabe11um <r.a.bazhenoff@gmail.com>
2025-07-09 15:26:42 +0300

    Map block added

--- resources/views/layouts/partials/map.blade.php
+++ resources/views/layouts/partials/map.blade.php
@@ -1,4 +1,11 @@
-<section class="w-screen h-screen" id="yandex-map"></section>
+<section class="w-screen h-screen relative" id="yandex-map">
+    <iframe
+        src="https://yandex.ru/map-widget/v1/org/chaochay/81239074993/?ll=37.611569%2C55.741480&z=18.27"
+        class="absolute top-0 left-0 w-full h-full"
+        frameborder="0"
+        allowfullscreen="true">
+    </iframe>
+</section>
 
 @push('scripts')
     <script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>

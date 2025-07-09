b6ebdd059c253d8b425174ad277724edf06cc708
Parabe11um <r.a.bazhenoff@gmail.com>
2025-07-09 15:29:03 +0300

    Map block added

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -26,6 +26,7 @@
                 <li class="nav-item"><a class="nav-link scroll hover:!text-[#28a745]" href="#tea-items">Наш чай</a></li>
                 <li class="nav-item"><a class="nav-link hover:!text-[#28a745]" href="{{ asset('menu.pdf') }}" target="_blank">Наше меню</a></li>
                 <li class="nav-item"><a class="nav-link scroll hover:!text-[#28a745]" href="#contact">Контакты</a></li>
+                <li class="nav-item"><a class="nav-link scroll hover:!text-[#28a745]" href="#yandex-map">Как добраться</a></li>
             </ul>
             <!-- /.navbar-nav -->
             <div class="offcanvas-footer xl:!hidden lg:!hidden">

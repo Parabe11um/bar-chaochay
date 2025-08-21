fc144b0b299d31acfd3069b3d8aea53bd5282321
Parabe11um <r.a.bazhenoff@gmail.com>
2025-08-21 19:16:34 +0300

    News block added

--- app/Filament/Admin/Resources/NewsResource/Pages/ListNews.php
+++ app/Filament/Admin/Resources/NewsResource/Pages/ListNews.php
@@ -3,7 +3,6 @@
 namespace App\Filament\Admin\Resources\NewsResource\Pages;
 
 use App\Filament\Admin\Resources\NewsResource;
-use App\Filament\Resources\NewsResource;
 use Filament\Resources\Pages\ListRecords;
 
 class ListNews extends ListRecords

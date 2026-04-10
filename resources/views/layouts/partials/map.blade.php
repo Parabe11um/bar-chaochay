5d4a6eda677bf647aedcc6d25b125e1a55d96b96
Parabe11um <r.a.bazhenoff@gmail.com>
2026-04-10 16:52:35 +0300

    contacts updated

--- resources/views/layouts/partials/map.blade.php
+++ resources/views/layouts/partials/map.blade.php
@@ -1,6 +1,6 @@
 <section class="w-screen h-screen relative" id="yandex-map">
     <iframe
-        src="https://yandex.ru/map-widget/v1/org/chaochay/81239074993/?ll=37.611569%2C55.741480&z=18.27"
+        src="https://yandex.ru/map-widget/v1/org/chaochay/168403993029/?ll=37.682683%2C55.772107&z=18.27"
         class="absolute top-0 left-0 w-full h-full"
         frameborder="0"
         allowfullscreen="true">
